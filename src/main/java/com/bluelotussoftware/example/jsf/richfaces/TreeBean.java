94032358acee8b659c7f9313d65096a07df7b7de
John Yeary <jyeary@bluelotussoftware.com>
2013-04-02 00:11:53 -0400

    Removed nodes.

--- src/main/java/com/bluelotussoftware/example/jsf/richfaces/TreeBean.java
+++ src/main/java/com/bluelotussoftware/example/jsf/richfaces/TreeBean.java
@@ -9,6 +9,8 @@ package com.bluelotussoftware.example.jsf.richfaces;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+import java.util.UUID;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
@@ -30,93 +32,64 @@ import org.richfaces.model.TreeNode;
 public class TreeBean implements Serializable {
 
     private static final long serialVersionUID = 3096479824299598082L;
-    private NodesTreeSequenceKeyModelImpl<TreeNodeImpl<String>> customModel;
     private ClassicTreeNodeDataModelImpl model;
-    private List<TreeNode> nodes;
+    private TreeNodesSequenceKeyModel<TypedTreeNode<String>> customModel;
+    private List<TypedTreeNode<String>> customNodes;
     private TreeNode selectedNode;
 
     public TreeBean() {
         model = new ClassicTreeNodeDataModelImpl();
-        customModel = new NodesTreeSequenceKeyModelImpl<TreeNodeImpl<String>>();
-        nodes = new ArrayList<TreeNode>();
+        customModel = new TreeNodesSequenceKeyModel<TypedTreeNode<String>>();
+        customNodes = new ArrayList<TypedTreeNode<String>>();
 
-        TreeNodeImpl<String> root = new TreeNodeImpl<String>();
-
-        TreeNodeImpl<String> parent = new TreeNodeImpl<String>();
-        parent.setData("John");
-
-        TreeNodeImpl<String> child1 = new TreeNodeImpl<String>();
-        child1.setData("Ethan");
-
-        TreeNodeImpl<String> child2 = new TreeNodeImpl<String>();
-        child2.setData("Sean");
-
-        root.addChild("John", parent);
-        parent.addChild("Ethan", child1);
-        parent.addChild("Sean", child2);
-
+        //MODEL
+        TypedTreeNode<String> root = new TypedTreeNode<String>("Family");
+        TypedTreeNode<String> father = new TypedTreeNode<String>("John");
+        TypedTreeNode<String> mother = new TypedTreeNode<String>("Patty");
+        TypedTreeNode<String> child1 = new TypedTreeNode<String>("Ethan");
+        TypedTreeNode<String> child2 = new TypedTreeNode<String>("Sean");
+        root.addChild("John", father);
+        root.addChild("Patty", mother);
+        father.addChild("Ethan", child1);
+        father.addChild("Sean", child2);
+        mother.addChild("Ethan", child1);
+        mother.addChild("Sean", child2);
         model.setWrappedData(root);
 
-        org.richfaces.model.TreeNodeImpl node = new org.richfaces.model.TreeNodeImpl();
-        org.richfaces.model.TreeNodeImpl node2 = new org.richfaces.model.TreeNodeImpl();
-        node.addChild("A", node2);
-        for (int i = 0; i < 10; i++) {
-            org.richfaces.model.TreeNodeImpl v = new org.richfaces.model.TreeNodeImpl();
-            node2.addChild(i, v);
-            nodes.add(v);
-        }
-
-        TreeNodeImpl<String> rNode = new TreeNodeImpl<String>();
-        rNode.setData("Root");
-        TreeNodeImpl<String> pNode1 = new TreeNodeImpl<String>();
-        pNode1.setData("Node 1");
-        TreeNodeImpl<String> pNode2 = new TreeNodeImpl<String>();
-        pNode2.setData("Node 2");
-        TreeNodeImpl<String> pNode3 = new TreeNodeImpl<String>();
-        pNode3.setData("Node 3");
-        TreeNodeImpl<String> pNode4 = new TreeNodeImpl<String>();
-        pNode4.setData("Node 4");
-        TreeNodeImpl<String> pNode5 = new TreeNodeImpl<String>();
-        pNode5.setData("Node 5");
-
-        rNode.addChild("pNode1", pNode1);
-        rNode.addChild("pNode2", pNode2);
-        rNode.addChild("pNode3", pNode3);
-        rNode.addChild("pNode4", pNode4);
-        rNode.addChild("pNode5", pNode5);
-
-        pNode1.addChild("pNode2", pNode2);
-//        pNode2.addChild("pNode3", pNode3);
-//        pNode3.addChild("pNode4", pNode4);
-//        pNode4.addChild("pNode5", pNode5);
-//        pNode5.addChild("pNode1", pNode1);
-
-        List<TreeNodeImpl<String>> list = new ArrayList<TreeNodeImpl<String>>();
-        list.add(rNode);
+        //CUSTOM MODEL
+        List<TypedTreeNode<String>> list = new ArrayList<TypedTreeNode<String>>();
 
         for (int i = 0; i < 10; i++) {
-            TreeNodeImpl<String> tNode = new TreeNodeImpl<String>();
-            tNode.setData(String.valueOf(i));
-            pNode1.addChild(i, tNode);
-            pNode2.addChild(i, tNode);
-            pNode3.addChild(i, tNode);
-            pNode4.addChild(i, tNode);
-            pNode5.addChild(i, tNode);
-            list.add(tNode);
+            TypedTreeNode<String> node = new TypedTreeNode<String>();
+            node.setData(UUID.randomUUID().toString());
+            list.add(node);
         }
-        customModel.setWrappedData(list);
+
+        TypedTreeNode<String> customRoot = new TypedTreeNode<String>();
+        Random r = new Random();
+        for (TypedTreeNode<String> node : list) {
+
+            for (int i = 0; i < list.size(); i++) {
+                node.addChild(UUID.randomUUID().toString(), new TypedTreeNode<String>("sub - " + r.nextInt(32768) + " - " + node.getData()));
+            }
+            customRoot.addChild(node.getData(), node);
+        }
+        customModel.setWrappedData(customRoot);
+
+        //NODES
+        customNodes.addAll(list);
     }
 
     public ClassicTreeNodeDataModelImpl getModel() {
         return model;
     }
 
-    public NodesTreeSequenceKeyModelImpl<TreeNodeImpl<String>> getCustomModel() {
+    public TreeNodesSequenceKeyModel<TypedTreeNode<String>> getCustomModel() {
         return customModel;
     }
 
-    public List<TreeNode> getNodes() {
-        return nodes;
+    public List<TypedTreeNode<String>> getCustomNodes() {
+        return customNodes;
     }
 
     public TreeNode getSelectedNode() {
@@ -135,7 +108,7 @@ public class TreeBean implements Serializable {
         Object storedKey = tree.getRowKey();
         tree.setRowKey(currentSelectionKey);
         selectedNode = (TreeNode) tree.getRowData();
-        System.out.println("A node was selected: " + ((TreeNodeImpl<String>) selectedNode).getData());
+        System.out.println("A node was selected: " + ((TypedTreeNode<String>) selectedNode).getData());
     }
 
     public void selectionChanged(TreeSelectionChangeEvent event) {
@@ -144,9 +117,5 @@ public class TreeBean implements Serializable {
 
     public void toggleChanged(TreeToggleEvent event) {
         FacesContext.getCurrentInstance().addMessage(event.getComponent().getClientId(FacesContext.getCurrentInstance()), new FacesMessage("TreeToggleEvent FIRED"));
-    }
-
-    public void crap() {
-        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("CRAP EVENT FIRED"));
     }
 }
